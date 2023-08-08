bd419313dba1272b862ea9f2e3c81738b3f0c7b0
snake324 <kristi4n90@gmail.com>
2023-08-08 12:53:48 +0200

    most popular added

--- src/app/modules/home/most-popular-restaurant/most-popular-restaurant.component.ts
+++ src/app/modules/home/most-popular-restaurant/most-popular-restaurant.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CitiesService } from 'src/app/services/cities.service';
 @Component({
   selector: 'app-most-popular-restaurant',
   templateUrl: './most-popular-restaurant.component.html',
   styleUrls: ['./most-popular-restaurant.component.scss']
 })
-export class MostPopularRestaurantComponent {
+export class MostPopularRestaurantComponent implements OnInit {
+  cities: any[] = [];
+  popularRestaurants: any[] = [];
 
+  constructor(private citiesService: CitiesService) {}
+
+  ngOnInit(): void {
+    this.citiesService.getCities().subscribe(data => {
+      this.cities = data.cities;
+      this.sortCitiesByPopularity();
+      const top4Cities = this.cities.slice(0, 4);
+      this.popularRestaurants = top4Cities.flatMap(city => city.popularRestaurants);
+    });
+  }
+
+  sortCitiesByPopularity() {
+    this.cities.sort((a, b) => +a.popular - +b.popular);
+  }
 }
